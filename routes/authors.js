8917bc6bef17b1b8e1e02e3422bbf930fefeaa54
Ghozy165 <labib20ti@mahasiswa.pcr.ac.id>
2025-05-25 11:48:02 +0700

    init author route

---
+++ routes/authors.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router(); 
+const Author = require('../models/author');
+
+// All Authors
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try { 
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', {
+            authors: authors,
+            searchOptions: req.query.name
+        })
+    } catch {
+        res.redirect('/')
+    }
+})
+
+// New Author
+router.get('/new', (req, res) => {
+    res.render('authors/new', {author: new Author()});
+})
+
+// Create Author
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    
+    try {
+        const newAuthor = await author.save()
+        console.log('Author created:', newAuthor)
+        res.redirect(`authors`)
+    } catch (err) {
+        console.error('Error creating author')
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating Author: ' + err.message
+        })
+    }
+})
+
+module.exports = router;
